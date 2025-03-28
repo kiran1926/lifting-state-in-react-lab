43704c73950786c9fae20b35e705c799716a7a80
kiran1926 <kiranroge19@gmail.com>
2025-03-28 01:11:41 -0500

    wip: added logic for adding ingredient to burger

---
+++ src/components/BurgerStack/BurgerStack.jsx
@@ -0,0 +1,24 @@
+
+
+
+
+const BurgerStack = (props) => {
+  return (
+    <div>
+        <h2>🍔 Your Burger: </h2>
+      <ul>
+        {props.stack.map((ingredient, index) => (
+          <li key={index} style={{ backgroundColor: ingredient.color }}>
+            <h2>{ingredient.name}</h2>
+            <button onClick={() => props.removeFromBurger(ingredient)}>
+            ❌
+            </button>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+  
+  export default BurgerStack;
+  
